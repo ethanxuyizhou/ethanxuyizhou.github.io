40c87a416583bc8837fb95f329087e3b3c990a3f
ethanxuyizhou <xuyizhou98@gmail.com>
2021-04-04 17:15:54 -0400

    z

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-ng deploy --base-href=https://ethanxuyizhou.github.io/ethanxuyizhou.github.io/ --name=ethanxuyizhou --email="yizhoux@andrew.cmu.edu"
+ng build --prod --base-href="ethanxuyizhou.github.io"
+ng deploy --base-href src
